ae16bc4c57f8899a7715bed3d91418b636162a18
Alec Chaprak <achaprak@gmail.com>
2023-12-09 17:19:48 -0800

    Added exersise 10 table need to revisit logos

---
+++ SdetPrep/JSFundamentalsReview/lessons/lesson5.js
@@ -0,0 +1,9 @@
+var cars = ["Volvo", "Toyota", "Tesla"];
+
+cars.map(car => {
+    console.log(car);
+})
+
+cars.forEach(car => {
+    console.log(car);
+})
---
+++ SdetPrep/JSFundamentalsReview/lessons/lesson6.js
@@ -0,0 +1,22 @@
+//Declarative functons
+
+function helloOne() {
+    console.log("Hello one")
+}
+
+helloOne();
+
+//Anoymus function
+var helloTwo = function() {
+    console.log("Hello two")
+
+}
+helloTwo();
+
+//es6 function / arrow function
+
+var hello = () => {
+    console.log("Hello arrow")
+}
+
+hello()
